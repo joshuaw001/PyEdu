04081eabaa1898c994b71b13cef63bc3a3d7c54a
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2020-08-11 12:31:38 -0500

    Update main.py

--- PyGeometry/main.py
+++ PyGeometry/main.py
@@ -1,3 +1,16 @@
+class WrongConfigError(Exception):
+    def __init__(self,name):
+        self.name = name
+        self.message = "configured option named {self.name} doesn't exist."
+        super().__init__(self.message)
 class PointStyle:
-    pass
-    
+    def __init__(self,**opts):
+        self.valid_opts = ["x","y","bg","label","size"]
+        self.configs    = {}
+        for i,j in opts:
+            if i in self.valid_opts:
+                self.configs[i] = j
+            else:
+                raise WrongConfigError(i)
+                break
+        zxdrg 
