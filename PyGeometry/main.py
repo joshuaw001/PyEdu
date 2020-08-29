5274b589d6a2dce06b1a77ccd67ce7ec701ac086
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2020-08-29 10:07:58 -0500

    Point class updated

--- PyGeometry/main.py
+++ PyGeometry/main.py
@@ -1,2 +1,19 @@
+import turtle as t
+
 class Point:
-    pass
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.style = None
+		self.rendered = False
+	def draw(self):
+		t.goto(self.x,self.y)
+		t.dot()
+		self.rendered = True
+	def __del__(self):
+		self.draw()
+		t.undo()
+		self.rendered = False
+
+class LineSegment:
+	pass
